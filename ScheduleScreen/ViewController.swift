4d3240eea332f33bb972b4d34c7a480eca54ed68
enotus <enotus@gmail.com>
2018-04-25 06:06:24 +0300

    Initial Commit

---
+++ ScheduleScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScheduleScreen
+//
+//  Created by Enotus on 25/04/2018.
+//  Copyright © 2018 enotus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
